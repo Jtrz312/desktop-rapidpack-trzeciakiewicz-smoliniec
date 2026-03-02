febf57ad0d8f015a9e0e8a50e8498a30653dfbed
mikolaj <mikolaj.smoliniec@cyfroweszkoly.pl>
2026-03-02 17:23:03 +0100

    Add early backend + parcel class, awaiting frontend data,

---
+++ RapidPack/Classes/ParcelCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AvaloniaApplication1.Classes;
+
+public class ParcelCalculator
+{
+    public int CalculatePrice(int parcelWidth, int parcelHeight,int parcelDepth, int parcelWeight, bool express, int deliveryChoice)
+    {
+        double basePrice = 10.0;
+        double finalPrice;
+        
+        if (parcelWidth + parcelHeight + parcelDepth > 150)
+        {
+            finalPrice = 1.5*(basePrice + parcelWeight * 2);
+        }
+        else
+        {
+            finalPrice = basePrice + parcelWeight * 2;
+        }
+        
+        if (express)
+        {
+            finalPrice += 10;
+        }
+
+        if (deliveryChoice == 1)
+        {
+            finalPrice += 10;
+        }
+        else if (deliveryChoice == 2)
+        {
+            finalPrice = 100;
+        }
+
+        Math.Floor(finalPrice);
+        return (int)finalPrice;
+    }
+}
--- RapidPack/MainWindow.axaml.cs
+++ RapidPack/MainWindow.axaml.cs
@@ -8,4 +8,36 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+    
+    /*
+    ParcelCalculator parcelCalculator = new ParcelCalculator();
+    private void HandleCalculation(object? sender, RoutedEventArgs e)
+    {
+        TextBox[] textBoxes = [HeightBox, WidthBox, DepthBox, WeightBox];
+        
+        if (textBoxes.All(x => int.TryParse(x.Text, out int result)))
+        {
+            int parcelHeight = int.Parse(HeightBox.Text);
+            int parcelWidth = int.Parse(WidthBox.Text);
+            int parcelDepth = int.Parse(DepthBox.Text);
+            int parcelWeight = int.Parse(WeightBox.Text);
+            bool expressChecked = ExpressBox.IsChecked ?? false;
+            int deliveryChoice = DeliveryBox.SelectedIndex;
+
+            if (parcelWeight <= 30)
+            {
+                int parcelPrice = parcelCalculator.CalculatePrice(parcelWidth, parcelHeight, parcelDepth, parcelWeight,expressChecked, deliveryChoice);
+                Console.WriteLine(parcelPrice);
+            }
+            else
+            {
+                Console.WriteLine("Waga przesyłki jest zbyt duża!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Not cool!");
+        }
+    }
+    */
 }

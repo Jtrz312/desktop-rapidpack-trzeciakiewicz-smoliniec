a35f2e6caf0ba1432e3adbc2c08d699a82d60972
msmoliniec <mikolaj.smoliniec@cyfroweszkoly.pl>
2026-03-03 16:00:48 +0100

    Add first test

--- RapidPack.Tests/MainWindowTests.cs
+++ RapidPack.Tests/MainWindowTests.cs
@@ -5,4 +5,17 @@ namespace RapidPack.Tests;
 
 public class MainWindowTests
 {
+
+    [AvaloniaFact]
+    public void TextBox_ShouldAlertWhenNull()
+    {
+        var window = new MainWindow();
+        var WidthBox = window.FindControl<TextBox>("WidthBox");
+        var Output = window.FindControl<TextBlock>("output");
+        var button = window.FindControl<Button>("Submit");
+        
+        WidthBox.Text = null;
+        
+        Assert.Equal("Podano nieprawidłowe/niepełne dane!",Output.Text);
+    }
 }
